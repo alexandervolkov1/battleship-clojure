a9c6dc7f0a5eec1fac348a35cc7d8b78fe364f19
alexandervolkov1 <a.volkov111555@gmail.com>
2026-09-16 16:51:54 +0300

    add shots

--- src/battleship/core.clj
+++ src/battleship/core.clj
@@ -1,11 +1,17 @@
 (ns battleship.core)
 
-;; constants
+;;===========================================================
+;; Constants
+;;===========================================================
 
 (def board-size 10)
 
 (def fleet-spec
   [4 3 3 2 2 2 1 1 1 1])
+
+;;===========================================================
+;; Board geometry
+;;===========================================================
 
 (defn valid-coordinate? [[x y]]
   (and (<= 0 x (dec board-size))
@@ -32,24 +38,88 @@
   (set
    (mapcat neighbor-cells cells)))
 
-(defn can-place-ship? [new-cells existing-cells]
-  (and
-   (valid-ship? new-cells)
-   (not-any? #(contains? (ship-neighborhood existing-cells) %)
-             new-cells)))
+;;============================================================
+;; Fleet
+;;===============================================================
 
 (defn fleet-cells [fleet]
   (set (mapcat :cells fleet)))
 
+(defn can-place-ship? [new-cells fleet]
+  (let [occupied (fleet-cells fleet)
+        forbidden (ship-neighborhood occupied)]
+    (and
+     (valid-ship? new-cells)
+     (not-any? #(contains? forbidden %) new-cells))))
 
+(defn place-ship [fleet start length orientation]
+  (let [new-ship (ship-cells start length orientation)]
+    (if (can-place-ship? new-ship fleet)
+      (conj fleet {:cells new-ship})
+      nil)))
 
-    
+;;===============================================================
+;; Shooting
+;;===============================================================
 
+(defn ship-at [fleet coordinate]
+  (some
+   #(when (contains? (:cells %) coordinate) %)
+   fleet))
+
+(defn hit? [fleet coordinate]
+  (some? (ship-at fleet coordinate)))
+
+(defn sunk? [ship shots]
+  (every? #(contains? shots %) (:cells ship)))
+
+(defn all-sunk? [fleet shots]
+  (every? #(sunk? % shots) fleet))
+
+(defn shot-result [fleet shots coordinate]
+  (let [new-shots (conj shots coordinate)]
+    (if
+
+;;===============================================================
+;; tests
+;;===============================================================
+  
 (comment
-  (ship-cells [5 7] 6 :horizontal)
-  (valid-ship? (ship-cells [8 7] 4 :vertical))
-  (valid-ship? (ship-cells [1 3] 5 :horizontal))
-  (neighbor-cells [1 1])
-  (ship-neiborhood #{[0 0] [1 1]})
-  (fleet-cells [{:cells #{[1 1] [2 1]}}
-                {:cells #{[5 5]}}]))
+
+  (def sample-fleet
+    [{:cells #{[0 0] [1 0] [2 0] [3 0]}}
+     {:cells #{[9 0] [9 1] [9 2]}}
+     {:cells #{[4 9] [5 9]}}
+     {:cells #{[9 9]}}])
+
+  ;; valid edge cases
+  (valid-ship? (ship-cells [0 0] 4 :horizontal))
+  (valid-ship? (ship-cells [9 7] 3 :vertical))
+  (valid-ship? (ship-cells [7 9] 3 :horizontal))
+
+  ;; outside board
+  (valid-ship? (ship-cells [8 5] 3 :horizontal))
+  (valid-ship? (ship-cells [4 9] 2 :vertical))
+
+  ;; fleet placement
+  (can-place-ship?
+   (ship-cells [5 4] 3 :horizontal)
+   sample-fleet)
+
+  ;; overlap
+  (can-place-ship?
+   (ship-cells [2 0] 3 :vertical)
+   sample-fleet)
+
+  ;; side contact
+  (can-place-ship?
+   (ship-cells [0 1] 2 :horizontal)
+   sample-fleet)
+
+  ;; diagonal contact
+  (can-place-ship?
+   #{[4 1]}
+   sample-fleet)
+
+  ;; add new ship
+  (place-ship sample-fleet [5 5] 2 :vertical))
